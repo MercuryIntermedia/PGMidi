f34b90c33fbc81f66c680c44a161b67e3947a010
Demetri Miller <dmiller@mercuryintermedia.com>
2015-03-13 16:56:16 -0500

    More ARCification

--- Sources/UI/MidiMonitorViewController.h
+++ Sources/UI/MidiMonitorViewController.h
@@ -11,28 +11,11 @@
 @class PGMidi;
 
 @interface MidiMonitorViewController : UIViewController
-{
-    UILabel    *countLabel;
-    UITextView *textView;
 
-    PGMidi *midi;
-}
-
-#if ! __has_feature(objc_arc)
-
-@property (nonatomic,retain) IBOutlet UILabel    *countLabel;
-@property (nonatomic,retain) IBOutlet UITextView *textView;
-
-@property (nonatomic,assign) PGMidi *midi;
-
-#else
-
-@property (nonatomic,strong) IBOutlet UILabel    *countLabel;
-@property (nonatomic,strong) IBOutlet UITextView *textView;
+@property (nonatomic, weak) IBOutlet UILabel    *countLabel;
+@property (nonatomic, weak) IBOutlet UITextView *textView;
 
 @property (nonatomic,strong) PGMidi *midi;
-
-#endif
 
 - (IBAction) clearTextView;
 - (IBAction) listAllInterfaces;
